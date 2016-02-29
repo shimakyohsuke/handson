72cf7d39841ee357f3b444aaff99bafd062d0afb
MATSUDAEmi <Emi@ariyoMerv.local>
2016-02-29 22:17:37 +0900

    README.md更新

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,60 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
-var browser = require("browser-sync");// ← browserという名前をつけてbrowser-syncを読み込み
+var browser = require("browser-sync");
+var jade = require('gulp-jade');
+var uglify = require("gulp-uglify");
+var imagemin = require("gulp-imagemin");
+var pngquant = require("imagemin-pngquant");
+var changed  = require('gulp-changed');
 
 gulp.task("sass", function() {
-    gulp.src("assets/scss/*scss")
+    gulp.src("./assets/scss/*scss")
         .pipe(sass())
-        .pipe(gulp.dest("public/css"))
+        .pipe(gulp.dest("./public/css"))
         .pipe(browser.reload({stream:true}));
 });
 
-gulp.task("server", function() {	// ← serverというタスクをつくります宣言。
+gulp.task('jade', function () {
+    gulp.src('./assets/tmpl/*jade')
+        .pipe(jade({
+            pretty: true
+        }))
+        .pipe(gulp.dest('./public'))
+        .pipe(browser.reload({stream:true}));
+});
+
+gulp.task("js", function() {
+    gulp.src("./assets/js/*.js")
+        .pipe(uglify())
+        .pipe(gulp.dest("./public/js"))
+        .pipe(browser.reload({stream:true}));
+});
+
+
+gulp.task("image", function() {
+    gulp.src("./assets/images/*.{png,jpg,gif}")
+        .pipe(changed("./public/images"))
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest("./public/images"))
+        .pipe(browser.reload({stream:true}));
+});
+
+
+
+gulp.task("server", function() {
     browser({
-        server: {	// ← 表示に使用するサーバを立てます
-            baseDir: "public"	// ← publicというディレクトリ以下を表示します
+        server: {
+            baseDir: "./public"
         }
     });
 });
 
-gulp.task("browser-sync",['server'], function() {	// ← sass-watchというタスクをbrowser-syncというタスクに変更し、実行する前にかならずserverタスクを実行します。
+gulp.task("default",['server'], function() {
     gulp.watch("assets/scss/*scss",["sass"]);
+    gulp.watch("assets/tmpl/*jade",["jade"]);
+    gulp.watch("assets/js/*js",["js"]);
+    gulp.watch("assets/images/*.{png,jpg,gif}",["image"]);
 });
